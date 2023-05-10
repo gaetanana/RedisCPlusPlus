a50d4f3fdabaf8c400610e7f9836ff6d89e43746
g.gonfiantini <gaetan.gonfiantini@gmail.com>
2023-05-10 12:06:11 +0200

    VIVE HIREDIS

--- main.cpp
+++ main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
+#include <hiredis/hiredis.h>
+#include <WinSock2.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const char* hostname = "localhost";
+    int port = 6379;
+    struct timeval timeout = { 1, 500000 }; // 1.5 secondes
+
+    std::cout << "Tentative de connexion a Redis...\n";
+
+    redisContext* c = redisConnectWithTimeout(hostname, port, timeout);
+    if (c == nullptr || c->err) {
+        std::cout << "Erreur de connexion à Redis.\n";
+        if (c) {
+            std::cout << "Message d'erreur : " << c->errstr << "\n";
+            redisFree(c);
+        } else {
+            std::cout << "Impossible d'allouer le contexte de redis.\n";
+        }
+        return 1;
+    }
+
+    std::cout << "Connecte a Redis! Envoi d'une commande PING...\n";
+
+    redisReply* reply = (redisReply*)redisCommand(c, "PING");
+    if (reply == nullptr) {
+        std::cout << "Erreur lors de l'envoi de la commande PING: " << c->errstr << "\n";
+        redisFree(c);
+        return 1;
+    }
+
+    std::cout << "Reponse a PING: " << reply->str << "\n";
+    freeReplyObject(reply);
+
+    std::cout << "Recuperation de toutes les cles...\n";
+
+    reply = (redisReply*)redisCommand(c, "KEYS *");
+    if (reply == nullptr) {
+        std::cout << "Erreur lors de l'envoi de la commande KEYS *: " << c->errstr << "\n";
+        redisFree(c);
+        return 1;
+    }
+
+    if (reply->type == REDIS_REPLY_ARRAY) {
+        for (int i = 0; i < reply->elements; i++) {
+            std::cout << "Cle : " << reply->element[i]->str << "\n";
+
+            redisReply* valueReply = (redisReply*)redisCommand(c, "GET %s", reply->element[i]->str);
+            if (valueReply != nullptr) {
+                std::cout << "Valeur : " << valueReply->str << "\n";
+                freeReplyObject(valueReply);
+            }
+        }
+    }
+
+    freeReplyObject(reply);
+    redisFree(c);
+
     return 0;
 }
