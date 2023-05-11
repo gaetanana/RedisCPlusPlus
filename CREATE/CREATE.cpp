f04d9bcade93c1375a4ee4e1c70fad058730e985
g.gonfiantini <gaetan.gonfiantini@gmail.com>
2023-05-11 16:19:48 +0200

    Je peux pas build pugixml car il me faut des droit administrateurs

--- CREATE/CREATE.cpp
+++ CREATE/CREATE.cpp
@@ -7,6 +7,17 @@
 #include <iostream>
 #include "CREATE.h"
 #include "..\ConnectionRedis\connection.h"
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <pugixml.hpp>
+#include <json/json.h>
+#include <hiredis/hiredis.h>
+
+class xml_document;
+
 using namespace std;
 
 /**
@@ -30,16 +41,63 @@ void createOneKeyValue(){
     fermertureRedis(c);
 }
 
+//TODO je dois installer la librairie jsoncpp
+/*string xmlToJson(string xml){
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_string(xml.c_str());
+    if (!result) {
+        std::cout << "Erreur lors du chargement du fichier XML : " << result.description() << "\n";
+        return "";
+    }
+
+    pugi::xml_node root = doc.document_element();
+    Json::Value rootJson;
+    pugi::xml_node child = root.first_child();
+    while (child) {
+        Json::Value childJson;
+        pugi::xml_attribute attr = child.first_attribute();
+        while (attr) {
+            childJson[attr.name()] = attr.value();
+            attr = attr.next_attribute();
+        }
+        rootJson[child.name()].append(childJson);
+        child = child.next_sibling();
+    }
+
+    Json::StreamWriterBuilder builder;
+    builder["indentation"] = "\t";
+    string jsonStr = Json::writeString(builder, rootJson);
+    return jsonStr;
+}*/
+
 /**
 * Cette fonction me permet de créer toutes les clés et valeurs d'un dossier dans la base de données Redis
  * En demandant le chemin absolu du dossier et tous les fichiers XML sont convertis en JSON et stockés dans la base de données Redis
  * Et le nom des clés est le nom du fichier XML
 */
 
-void createAllKeyValue(){
-    redisContext *c = connectionRedis();
+void createAllKeyValue() {
+    redisContext* c = connectionRedis();
     string path;
+
     cout << "Saisir le chemin absolu du dossier : ";
     cin >> path;
-    fermertureRedis(c);
+
+    for (const auto & entry : fs::directory_iterator(path)){
+        if (entry.path().extension() == ".xml"){
+            ifstream inFile;
+            inFile.open(entry.path());
+
+            stringstream strStream;
+            strStream << inFile.rdbuf(); // read the file
+            string xmlStr = strStream.str(); // str holds the content of the file
+
+            string jsonStr = xmlToJson(xmlStr);
+
+            // Save jsonStr in Redis with the key being the file name
+            string key = entry.path().stem().string();
+            redisCommand(c,"SET %s %s", key.c_str(), jsonStr.c_str());
+        }
+    }
+    redisFree(c);
 }
