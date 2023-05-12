0435fa2997fc092f5eb759f65f695a4e874dff36
g.gonfiantini <gaetan.gonfiantini@gmail.com>
2023-05-12 15:43:45 +0200

    Ajout de pugixml2

---
+++ DriverPugiXML/pugixml2/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/g.gonfiantini/Desktop/RedisCPlusPlus/DriverPugiXML/pugixml
+# Build directory: C:/Users/g.gonfiantini/Desktop/RedisCPlusPlus/DriverPugiXML/pugixml/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
