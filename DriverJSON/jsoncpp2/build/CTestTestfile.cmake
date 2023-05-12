f847cc8ab1dd09a00ffaf6cf79ae4e9e3426d592
g.gonfiantini <gaetan.gonfiantini@gmail.com>
2023-05-12 15:36:19 +0200

    ajout jsoncpp2

---
+++ DriverJSON/jsoncpp2/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/g.gonfiantini/Desktop/RedisCPlusPlus/DriverJSON/jsoncpp
+# Build directory: C:/Users/g.gonfiantini/Desktop/RedisCPlusPlus/DriverJSON/jsoncpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("include")
---
+++ DriverJSON/jsoncpp2/build/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/g.gonfiantini/Desktop/RedisCPlusPlus/DriverJSON/jsoncpp/include
+# Build directory: C:/Users/g.gonfiantini/Desktop/RedisCPlusPlus/DriverJSON/jsoncpp/build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
